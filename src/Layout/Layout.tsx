580d3f36009fca836ec4b043c88d52a48a2e1681
David Botello <dabocher@gmail.com>
2023-02-18 21:32:39 +0100

    Add Layout component

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+const Layout = (): JSX.Element => {
+  return (
+    <>
+      <header>
+        <h1>The Robots</h1>
+      </header>
+      <main></main>
+    </>
+  );
+};
+
+export default Layout;
