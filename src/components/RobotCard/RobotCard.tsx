f78c82d7e49d3ccd5f6bad92e598f774a280e461
DI3BOSS <diegofbermudez@gmail.com>
2023-02-18 18:59:52 +0100

    Implement RobotCardStyles

--- src/components/RobotCard/RobotCard.tsx
+++ src/components/RobotCard/RobotCard.tsx
@@ -1,4 +1,5 @@
 import { RobotStructure } from "../../types";
+import { RobotCardStyled } from "./RobotCardStyled";
 
 interface RobotCardProps {
   robot: RobotStructure;
@@ -6,26 +7,28 @@ interface RobotCardProps {
 
 const RobotCard = ({
   robot: {
-    id: { id },
+    _id: { $oid },
     name,
     image,
-    stats: { endurance, speed, dataOfCreation },
+    stats: { endurance, speed, dateOfCreation },
   },
 }: RobotCardProps): JSX.Element => {
   return (
     <>
-      <article className="robot-card">
-        <h2>err0R</h2>
-        <img
-          src="https://previews.123rf.com/images/funwayillustration/funwayillustration1609/funwayillustration160900436/63707662-robot-azul-ilustraci%C3%B3n-error-de-dise%C3%B1o.jpg"
-          alt="a robot"
-        />
-        <ul className="robot-stats">
-          <li>Speed: 0 / 10</li>
-          <li>Endurence: 7 / 10</li>
-          <li>Date of creation: 12/05/2078</li>
-        </ul>
-      </article>
+      <RobotCardStyled>
+        <article className="robot-card">
+          <h2>err0R</h2>
+          <img
+            src="https://previews.123rf.com/images/funwayillustration/funwayillustration1609/funwayillustration160900436/63707662-robot-azul-ilustraci%C3%B3n-error-de-dise%C3%B1o.jpg"
+            alt="a robot"
+          />
+          <ul className="robot-card__stats">
+            <li>Speed: 0 / 10</li>
+            <li>Endurence: 7 / 10</li>
+            <li>Date of creation: 12/05/2078</li>
+          </ul>
+        </article>
+      </RobotCardStyled>
     </>
   );
 };
