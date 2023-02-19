f41e84fbda2475363b807dbeb60882d14ebb2064
David Botello <dabocher@gmail.com>
2023-02-19 16:23:22 +0100

    Create Navbar styles

---
+++ src/components/Navbar/NavbarStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const NavbarStyled = styled.nav`
+  .navbar-list {
+    display: flex;
+    justify-content: space-between;
+    font-size: 3rem;
+    gap: 5rem;
+    color: #3567dc;
+  }
+`;
+
+export default NavbarStyled;
