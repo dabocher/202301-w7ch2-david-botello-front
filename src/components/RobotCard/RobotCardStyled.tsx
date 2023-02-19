4e90d109128d310b6bd0c10c23ad8af14299f6d1
David Botello <dabocher@gmail.com>
2023-02-19 12:02:33 +0100

    Add styles to RobotCard

--- src/components/RobotCard/RobotCardStyled.tsx
+++ src/components/RobotCard/RobotCardStyled.tsx
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
-export const RobotCardStyled = styled.li`
-  .robot-card {
-    display: flex;
-    flex-direction: column;
+export const RobotCardStyled = styled.article`
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+  width: 32rem;
+  box-shadow: 1px 2px 4px #0003;
+  border-radius: 10px;
+  background-color: white;
+  padding: 2rem 1rem;
+  gap: 1rem;
+  color: #333;
 
+  .robot-card {
+    &__title {
+      text-align: center;
+      width: 100%;
+      font-size: 3rem;
+    }
     &__stats {
+      font-size: 2rem;
       display: flex;
       flex-direction: column;
       justify-content: center;
