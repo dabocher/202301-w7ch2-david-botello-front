04f4e263c3a201f1d977e0e85e3f5ab5549c85fc
Sergio <sergiot.fontal@gmail.com>
2023-02-19 19:07:26 +0100

    Import useApi in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import Layout from "./Layout/Layout";
+import { useEffect } from "react";
+import useApi from "./hooks/useApi";
 
 const App = (): JSX.Element => {
+  const { getRobots } = useApi();
+
+  useEffect(() => {
+    getRobots();
+  }, [getRobots]);
+
   return <Layout />;
 };
 
