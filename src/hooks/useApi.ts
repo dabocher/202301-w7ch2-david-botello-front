11a8039e3c27236b4c2c4b6e48394c42ea395a1d
Sergio <sergiot.fontal@gmail.com>
2023-02-19 18:55:32 +0100

    Create custom hook useApi

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,19 @@
+import { useCallback } from "react";
+import { loadRobotsActionCreator } from "../store/features/robots/robotsSlice";
+import { useAppDispatch } from "../store/hooks";
+import { ApiResponseStructure } from "../types";
+
+const useApi = () => {
+  const dispatch = useAppDispatch();
+
+  const getRobots = useCallback(async () => {
+    const response = await fetch(process.env.REACT_APP_URL!);
+    const { robots } = (await response.json()) as ApiResponseStructure;
+
+    dispatch(loadRobotsActionCreator(robots));
+  }, [dispatch]);
+
+  return { getRobots };
+};
+
+export default useApi;
